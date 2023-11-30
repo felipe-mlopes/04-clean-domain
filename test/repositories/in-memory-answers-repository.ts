f07f6dac8792bd9075f2d33e7da42002495939fc
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-11-30 07:08:33 -0300

    test: add methods findById, save and delete

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -4,7 +4,29 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer'
 export class InMemoryAnswersRepository implements AnswersRepository {
   public item: Answer[] = []
 
+  async findById(id: string) {
+    const answer = this.item.find((item) => item.id.toString() === id)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
+
   async create(answer: Answer) {
     this.item.push(answer)
+  }
+
+  async save(answer: Answer) {
+    const itemIndex = this.item.findIndex((item) => item.id === answer.id)
+
+    this.item[itemIndex] = answer
+  }
+
+  async delete(answer: Answer) {
+    const itemIndex = this.item.findIndex((item) => item.id === answer.id)
+
+    this.item.splice(itemIndex, 1)
   }
 }
