f07b7970ff8e0441ca3a4c8af2e8632230f75b16
Felipe Lopes <felipe.mlopes03@gmail.com>
2023-12-18 10:13:00 -0300

    refactor: change path files error

--- src/domain/forum/application/use-cases/errors/not-allowed-error.ts
+++
@@ -1,7 +0,0 @@
-import { UseCaseError } from "@/core/erros/use-case-error";
-
-export class NotAllowedError extends Error implements UseCaseError {
-    constructor() {
-        super('Not allowed.')
-    }
-}
--- src/domain/forum/application/use-cases/errors/resource-not-found-error.ts
+++
@@ -1,7 +0,0 @@
-import { UseCaseError } from "@/core/erros/use-case-error";
-
-export class ResourceNotFoundError extends Error implements UseCaseError {
-    constructor() {
-        super('Resource not found.')
-    }
-}
